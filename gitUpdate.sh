359e40dd0261826b3e327e996536a7af50a86b60
Dominic Bett <dominicbett@hotmail.com>
2015-12-26 13:18:40 -0500

    fixing the broken bashfile

--- gitUpdate.sh
+++ gitUpdate.sh
@@ -1,7 +1,7 @@
 # Shell script which when run updates git
 # Usage: ./gitUpdate.sh
 #!/bin/bash
-git pull origin version2
+git pull git@github.com:bettkd/KenyaNews.git version2
 git add -A
 read -e -p "Please enter the commit message: " commit_message 
 echo "Commit message is - $commit_message."
